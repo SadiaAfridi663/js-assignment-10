530a269915e35b12ed78f9e0df2d7c01e3de0913
SadiaAfridi663 <sadiaafridi663@gmail.com>
2024-07-07 23:39:54 +0500

    problem 9,10

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// // problem 6
+// // problem 6 :   FIND THE SMALLEST NUMBER IN AN ARRAY
 // var numbers = []
 // for (var i = 0; i < 5; i++){
 //     var userInput = +prompt("please enter a number")
@@ -12,17 +12,42 @@
 // }
 // console.log({smallestNum})
 
-// problem 7
-// var celsius = +prompt("please enter temprature in celsius: ")
 
+// problem 7 : CONVERT CELCIUS TO FAHRENHEIT
+// var celsius = +prompt("please enter temprature in celsius: ")
 // var fahrenheit = (celsius * 9 / 5) + 32;
 // console.log(fahrenheit)
 
-// problem 8
-// var fahrenheit = +prompt("please enter temprature in fahrenheit: ")
 
+// problem 8 :  CONVERT FAHRENHEIT TO CELCIUS
+// var fahrenheit = +prompt("please enter temprature in fahrenheit: ")
 // var celsius =( fahrenheit - 32) * (5 / 9);
 // console.log(celsius)
 
 
+// problem 9 : REVERSE AN ARRAY using For loop
+// var array = [];
+// var reversedArray = []
+// for (var i = 0;i < 5;i++){
+//     var userInput = +prompt("please enter a number")
+//     array.push(userInput)
+// }
+// for (var i = array.length-1; i >=0; i-- ){
+//     reversedArray.push(array[i])
+// }
+// console.log(reversedArray)
 
+
+// problem 10 : CALCULATE AVERAGE OF AN ARRAY
+// var numbers = [];
+// var sum = 0;
+// var average = 0;
+// for (var i = 0;i < 5; i++){
+//     var userInput = +prompt("please enter a number")
+//     numbers.push(userInput)
+// }
+// for (var i = 0;i < 5;i++){
+//     sum += numbers[i];
+//     average = sum / numbers.length;
+// }
+// console.log({average})
