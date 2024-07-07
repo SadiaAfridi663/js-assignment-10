268e2fb189e332a3b0f3dc46126fea68f1d6a687
SadiaAfridi663 <sadiaafridi663@gmail.com>
2024-07-07 23:00:48 +0500

    problem 6,7,8

---
+++ index.js
@@ -0,0 +1,28 @@
+// // problem 6
+// var numbers = []
+// for (var i = 0; i < 5; i++){
+//     var userInput = +prompt("please enter a number")
+//     numbers.push(userInput)
+// }
+// var smallestNum = numbers[0]
+// for (var i = 0; i < 5; i++){
+//     if(numbers[i] < smallestNum){
+//         smallestNum = numbers[i]
+//     }
+// }
+// console.log({smallestNum})
+
+// problem 7
+// var celsius = +prompt("please enter temprature in celsius: ")
+
+// var fahrenheit = (celsius * 9 / 5) + 32;
+// console.log(fahrenheit)
+
+// problem 8
+// var fahrenheit = +prompt("please enter temprature in fahrenheit: ")
+
+// var celsius =( fahrenheit - 32) * (5 / 9);
+// console.log(celsius)
+
+
+
